5b6f05280948fef38a446776eaa025402ccb0183
MrRHINOPLAYZ <78490879+MrRHINOPLAYZ@users.noreply.github.com>
2021-05-16 20:10:19 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,34 @@
+var gameState = 0
+var playerCount = 0
+var database 
+var form,player,game
+var allPlayers
+var car1
+var car2,car3,car4
+var cars
+var red,blue,yellow,pink,track,welcome
+function preload() {
+    red=loadImage("red.png")
+    blue=loadImage("cyan.png")
+    pink=loadImage("pink.png")
+    yellow=loadImage("yellow.png")
+    track=loadImage("track.jpg")
+    welcome=loadImage("welcome.jpg")
+}
+function setup (){
+    createCanvas(displayWidth,displayHeight)
+    database = firebase.database()
+    game=new Game()
+    game.getState()
+    game.start()
+}
+function draw(){
+    background(welcome)
+if(playerCount===4){
+    game.update(1)
+}
+if (gameState===1){
+    clear()
+        game.play()
+}
+}
